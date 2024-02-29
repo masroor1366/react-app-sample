685841de899aef86d0daa4369b8c7f5437603e8e
masroor <masroor.1388@gmail.com>
2024-03-01 00:36:28 +0330

    حذف کاربر با متود DELETE با Axios

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -8,100 +8,14 @@ const Users = ()=>{
 
 
 
-            // const func = ()=>{
-        //     return new Promise((resolve , reject)=>{
-
-        //         console.log(1);
-
-        //         setTimeout(()=>{
-        //             console.log(2);
-        //             resolve(true)
-        //         } , 1000)
-
-        //     })
-        // }
-
-        // const test = async ()=>{
-        //     const res = await func();
-
-        //     if (res) {
-        //         console.log(3);
-        //     }
-        // }
-
-        // test();
-        const prom = (id)=>{
-            return axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
-        }
-
-        const func = async (id)=>{
-
-            const res = await prom(id);
-            console.log(res.data);
-
-            // await prom(id).then(res=>{
-            //     console.log(res.data);
-            // });
-            console.log(id);
-        }
-
-        // const test = (id)=>{
-        //     axios.get(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=>{
-        //         console.log(res.data);
-        //     });
-        //     console.log(id);
-        // }
-
-
-        for (const item of [1,2,3,4,5,6]) {
-
-            func(item);
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // axios.get('https://jsonplaceholder.typicode.com/users').then(res=>{
-        //     setUsers(res.data);
-        // }).catch(err=>{
-        //     console.log(err);
-        // })
-
-
     const [users,setUsers]=useState([]);
 
 
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/users').then(res=>{
             let myres = res.data;
-            console.log(myres)
             myres=[...myres,{id:66,name: 'Mohammad Ali Masroor', username:'Masroor1366',email:'Masroor.1388@gmail.com'}]
-            console.log(myres)
              setUsers(myres)
-            //  setUsers(...users,{id:141,name: 'Mohammad Ali Masroor', username:'masroor1366',email:'masroor.1388@gmail.com'})
 
              
         }).catch(err=>{
@@ -113,25 +27,50 @@ const Users = ()=>{
     console.log('param پارامتری که از زدن دکمه برگشت در اینجا دریافت می وشد')
     console.log(param)
 
-    const handleDelete = (itemId)=>{
+    const handleDelete = (itemId,username)=>{
         swal({
-            title: "https://sweetalert2.github.io/    حذف رکورد !",
-            text: `    آیا از حذف رکورد ${itemId} اطمینان دارید؟`,
+            title: "حذف رکورد !",
+            text: `آیا از حذف رکورد ${username} اطمینان دارید؟`,
             icon: "warning",
-            buttons: true,
+            buttons: ["خیر" , "بله"],
             dangerMode: true,
           })
           .then((willDelete) => {
             if (willDelete) {
               swal("حذف با موفقیت انجام شد", {
                 icon: "success",
+                buttons: "متوجه شدم",
+
               });
+                axios({
+                    method:"DELETE",
+                    url:`https://jsonplaceholder.typicode.com/users/${itemId}`
+                }).then(res=>{
+
+                    if (res.status == 200) {
+                        const newUsers = users.filter(u=>u.id != itemId);
+                        setUsers(newUsers);
+                        swal("حذف با موفقیت انجام شد", {
+                            icon: "success",
+                            buttons: "متوجه شدم",            
+                        });
+                    }else{
+                        swal("عملیات با خطا مواجه شد",{
+                            icon:"error",
+                            button:"متوجه شدم"
+                        });
+                    }
+
+                })
+
+
             } else {
               swal("شما از حذف رکورد منصرف شدید");
             }
           });
     }
 
+           
     return (
         <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
             <h4 className="text-center">مدیریت کاربران</h4>
@@ -175,10 +114,10 @@ const Users = ()=>{
                                     return navigate("/user/add/2",{state:{x:2,y:3}})
                                 }}                            
                             ></i>
-                            <Link to="/user/add" state={"vue"}>
+                             
                             <i className="fas fa-trash text-danger mx-2 pointer"
-                            onClick={()=>handleDelete(1)}></i>
-                            </Link>
+                            onClick={()=>handleDelete(u.id,u.username)}></i>
+                             
                         </td>
                     </tr>
                 ))}
